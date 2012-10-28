6a81aa8e63d110728cc4b5dd6506be8cfa0bdbe1
Mark Smith <mark.smith@practicalpoetry.co.uk>
2012-10-28 14:05:27 +0000

    Fixed args example.

--- examples/args.py
+++ examples/args.py
@@ -6,11 +6,13 @@ import os
 
 sys.path.insert(0, os.path.abspath('..'))
 
-from clint import args
+from clint import arguments
 from clint.textui import puts, colored, indent
 
+args = arguments.Args()
+
 with indent(4, quote='>>>'):
-    puts(colored.red('Aruments passed in: ') + str(args.all))
+    puts(colored.red('Arguments passed in: ') + str(args.all))
     puts(colored.red('Flags detected: ') + str(args.flags))
     puts(colored.red('Files detected: ') + str(args.files))
     puts(colored.red('NOT Files detected: ') + str(args.not_files))
