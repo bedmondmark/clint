8af88db0dbffd1d457b212044d150e0ce92947f1
Mark Smith <mark.smith@practicalpoetry.co.uk>
2012-10-28 14:19:13 +0000

    Can call get_each_args.sh from outside examples.

--- examples/get_each_args.sh
+++ examples/get_each_args.sh
@@ -1,4 +1,6 @@
+EXAMPLES=$(dirname $0) 
+
 echo "python get_each_args.py --name kracekumar --email me@kracekumar.com"
-python get_each_args.py --name kracekumar --email me@kracekumar.com
+python $EXAMPLES/get_each_args.py --name kracekumar --email me@kracekumar.com
 echo "python get_each_args.py --languages python c html ruby  --email me@kracekumar.com"
-python get_each_args.py --langauges python c html ruby  --email me@kracekumar.com
+python $EXAMPLES/get_each_args.py --langauges python c html ruby  --email me@kracekumar.com
