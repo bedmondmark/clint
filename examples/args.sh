8a90c500a96f4b274d7aa569924ed30da19a7df1
Mark Smith <mark.smith@practicalpoetry.co.uk>
2012-10-28 14:18:31 +0000

    Better var name.

--- examples/args.sh
+++ examples/args.sh
@@ -1,15 +1,16 @@
-HERE=$(dirname $0)
+EXAMPLES=$(dirname $0)
+
 echo 'No arguments:'
-python $HERE/args.py 
+python $EXAMPLES/args.py 
 
 echo 'A few arguments:'
-python $HERE/args.py -s this that --import this and that and this and that
+python $EXAMPLES/args.py -s this that --import this and that and this and that
 
 echo 'A few expanded file arguments:'
-python $HERE/args.py *.py
+python $EXAMPLES/args.py *.py
 
 echo 'A few non-expanded file arguments:'
-python $HERE/args.py '*.py'
+python $EXAMPLES/args.py '*.py'
 
 echo 'A few mixed files/flags/arguments:'
-python $HERE/args.py '*.py' --test test face book -s ~/.ssh
+python $EXAMPLES/args.py '*.py' --test test face book -s ~/.ssh
