7bf1969f972a8eef92fe12d060175a0ee42075ab
Mark Smith <mark.smith@practicalpoetry.co.uk>
2012-10-28 14:13:43 +0000

    Can now run args.sh from outside the examples dir.

--- examples/args.sh
+++ examples/args.sh
@@ -1,15 +1,15 @@
-
+HERE=$(dirname $0)
 echo 'No arguments:'
-python args.py 
+python $HERE/args.py 
 
 echo 'A few arguments:'
-python args.py -s this that --import this and that and this and that
+python $HERE/args.py -s this that --import this and that and this and that
 
 echo 'A few expanded file arguments:'
-python args.py *.py
+python $HERE/args.py *.py
 
 echo 'A few non-expanded file arguments:'
-python args.py '*.py'
+python $HERE/args.py '*.py'
 
 echo 'A few mixed files/flags/arguments:'
-python args.py '*.py' --test test face book -s ~/.ssh
+python $HERE/args.py '*.py' --test test face book -s ~/.ssh
